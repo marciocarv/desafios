5d7fc52280aaf939090286944558a981e79cf516
unknown <marciotibapa@gmail.com>
2022-05-05 22:05:07 -0300

    criando field

---
+++ 03 - battleship/battleship.js
@@ -0,0 +1,15 @@
+let hits = 0;
+let bombs = 0;
+
+
+function createFields(){
+    for(let i = 0; i<10; i++){
+        for(let j = 0; j<10; j++){
+            console.log('x'+i+'y'+j);
+        }
+    }
+}
+
+createFields();
+
+console.log("teste");
