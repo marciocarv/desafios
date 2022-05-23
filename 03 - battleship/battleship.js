32e1546c175ef86e5377263aa10ad3a15860fbec
unknown <marciotibapa@gmail.com>
2022-05-22 23:16:32 -0300

    melhoras no layout e cliques nos campos

--- 03 - battleship/battleship.js
+++ 03 - battleship/battleship.js
@@ -1,8 +1,14 @@
-let hits = 0;
-let bombs = 0;
-let attempts = 70;
+let hit = 0;
+let hits = document.querySelector('#hits');
+let attempts = 10;
+let atts = document.querySelector('#atts');
 let battleField = document.querySelector('#battleField');
 let board = [];
+let fields = [];
+let message = document.querySelector('.message');
+let score = document.querySelector('#score');
+let resultGame = true;
+let reset;
 
 const ships = [
     {
@@ -33,19 +39,21 @@ const ships = [
 ]
 
 function createFields(){
+    atts.innerHTML = attempts;
+    hits.innerHTML = hit;
     for(let i = 0; i<10; i++){
         for(let j = 0; j<10; j++){
-            //let id = 'field'.concat(',', i.toString(), ',',j.toString());
             let id = 'field'.concat(parseInt(i+''+j));
             let field = document.createElement('div');
             field.setAttribute('id', id);
             field.setAttribute('class', 'field');
+            field.classList.add('water');
             battleField.appendChild(field);
             board.push(
                 {
                     x: i,
                     y: j,
-                    value: 'agua' 
+                    value: 'water' 
                 }
             );
         }
@@ -63,8 +71,6 @@ function setShips(){
 function sort(ship){
     let startFieldx = Math.floor(Math.random() * 10);
     let startFieldy = Math.floor(Math.random() * 10);
-
-    console.log(startFieldx, startFieldy);
     let direction = Math.floor(Math.random() * 2);
 
     fieldsChecked = check(startFieldx, startFieldy, direction, ship);
@@ -74,9 +80,6 @@ function sort(ship){
     }else{
         for(let i=0; i<ship.size; i++){
             board[fieldsChecked[i]].value = ship.name;
-            console.log(fieldsChecked);
-            document.querySelector('#'+'field'.concat(fieldsChecked[i])).classList.add(ship.name);
-            //console.log(document.querySelector('#'+'field'.concat(fieldsChecked[i])));
         }
     }
 
@@ -87,16 +90,16 @@ function check(startFieldx, startFieldy, direction, ship){
     let fieldsChecked = [];
     if(direction == 0){ //horizontal
         if(startFieldy + ship.size - 1 > 9){
-            console.log('não cabe');
+            //console.log('não cabe');
             return false; // não cabe
         }
         // cabe
         for(let i = 0; i<ship.size; i++){
-            if(board[parseInt(startFieldx+''+(startFieldy+i))].value === 'agua'){
+            if(board[parseInt(startFieldx+''+(startFieldy+i))].value === 'water'){
                 fieldsChecked.push(parseInt(startFieldx+''+(startFieldy+i)));
-                console.log('vazio');
+                //console.log('vazio');
             }else{
-                console.log('ocupado');
+                //console.log('ocupado');
                 return false;
             }
         }
@@ -104,16 +107,16 @@ function check(startFieldx, startFieldy, direction, ship){
         
     }else{ //vertical
         if(startFieldx + ship.size - 1 > 9){
-            console.log('não cabe');
+            //console.log('não cabe');
             return false; // não cabe
         }
         // cabe
         for(let i = 0; i<ship.size; i++){
-            if(board[parseInt((startFieldx+i)+''+startFieldy)].value === 'agua'){
+            if(board[parseInt((startFieldx+i)+''+startFieldy)].value === 'water'){
                 fieldsChecked.push(parseInt(startFieldx+i+''+startFieldy));
-                console.log('vazio');
+                //console.log('vazio');
             }else{
-                console.log('ocupado');
+                //console.log('ocupado');
                 return false;
             }
         }
@@ -121,9 +124,75 @@ function check(startFieldx, startFieldy, direction, ship){
     }
 }
 
+function endGame(result){
+    if(result == 'win'){
+        message.innerHTML = '';
+        score.innerHTML = '';
+        score.innerHTML += '<p class=\"win\">You Win!</p>';
+        score.innerHTML += '<p><button class=\"reset\" id=\"reset\">reset game</button></p>';
+        resultGame = false;
+    }else{
+        message.innerHTML = '';
+        score.innerHTML = '';
+        score.innerHTML += '<p class=\"lost\">You Lost!</p>';
+        score.innerHTML += '<p><button class=\"reset\" id=\"reset\">reset game</button></p>';
+        resultGame = false;
+    }
+    reset = document.querySelector('#reset');
+    reset.addEventListener('click', resetGame());
+}
+
+function resetGame(){
+    document.querySelector
+    hit = 0;
+    hits = document.querySelector('#hits');
+    attempts = 70;
+    atts = document.querySelector('#atts');
+    battleField.innerHTML = '';
+    board = [];
+    fields = [];
+    message = document.querySelector('.message');
+    score = document.querySelector('#score');
+    resultGame = true;
+    createFields();
+    setShips();
+}
+
 createFields();
-
-
 setShips();
+
+fields = document.querySelectorAll('.field');
+
+fields.forEach((item, index)=>{
+    item.addEventListener('click', ()=>{
+        if(!resultGame){
+            return false;
+        }
+        if(item.classList[1] != 'waterHited'){
+            attempts--;
+            atts.innerHTML = attempts;
+        }
+        if(board[index].value === 'water'){
+            message.style.color = "rgb(241, 116, 116)";
+            message.innerHTML = 'Hit the water :(';
+            item.classList.remove('water');
+            item.classList.add('waterHited');
+            item.innerHTML = 'X';
+        }else{
+            message.style.color = "rgb(141, 240, 207)";
+            message.innerHTML = 'Hit the '+board[index].value;
+            item.classList.remove('water');
+            item.classList.add(board[index].value);
+            hit++;
+            hits.innerHTML = hit;
+        }
+        if(attempts == 0 && hit < 35){
+            endGame('lost');
+        }
+        if(hit == 35){
+            endGame('win');
+        }
+    })
+});
 
 console.log(board);
